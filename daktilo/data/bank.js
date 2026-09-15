b1a5f184c3ae2a89833e9ba0e29507117d407d83
SwantjeHansen <84096838+SwantjeHansen@users.noreply.github.com>
2026-09-15 07:25:14 +0200

    Delete daktilo directory

--- daktilo/data/bank.js
+++
@@ -1,8 +0,0 @@
-window.DAKTILO_WORDS = {
-  easy: window.DAKTILO_EASY || { subcategories: {} },
-  hard: window.DAKTILO_HARD || { subcategories: {} },
-  technical: window.DAKTILO_TECHNICAL || { subcategories: {} },
-  english: window.DAKTILO_ENGLISH || { subcategories: {} },
-  names: window.DAKTILO_NAMES || { subcategories: {} },
-  sentences: window.DAKTILO_SENTENCES || { subcategories: {} }
-};
